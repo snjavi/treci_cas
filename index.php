1b42d98b6a82bf7439fc5139cd6ade8d8f5e7af3
snjavi <just.cheri7@gmail.com>
2018-03-28 19:19:47 +0200

    komentar2

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
+	$rezultat = str_replace("a","4","Ja sam programer");
+	echo "<br>".$rezultat;
+
+	$drugaRec = str_replace(["a", "e"], ["4","3"], "Ja sam programer");
+	echo "<br>".$drugaRec;
+
+	$malaSlova = "OVO BI TREBALO DA BUDE MALIM SLOVIMA";
+	$malaSlova = strtolower ($malaSlova);
+	echo "<br>".$malaSlova;
+
+
+	//strtoupper - mala za velika slova
+
+	$promena = str_replace(["a", "e", "i", "o"], ["4", "3", "1", "0"], "Ideja nije moja, smislili su je neki kreativni nastavnici");
+	echo "<br>".$promena;
+	$promena = strtoupper($promena);
+	echo "<br>".$promena;
+
 
 ?>
